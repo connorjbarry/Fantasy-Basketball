a145cd48f8c235231211f9e6567c875e97f1865a
connorjbarry <barry31@purdue.edu>
2022-04-28 02:02:36 -0400

    added new league form, with password checking

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 'use strict';
 
-const switcher = document.querySelector('.btn');
+const openFormbtn = document.querySelector('.openForm-btn');
+openFormbtn.addEventListener('click', function() {
+   openForm();
+});
 
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
+const closeFormbtn = document.querySelector(".cancel-btn");
+closeFormbtn.addEventListener('click', function() {
+    closeForm();
+});
 
-    /*var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
+const submitLeague = document.querySelector(".submit-btn")
+submitLeague.addEventListener('click', function() {
+    checkLeaguePasswordMatch();
+});
+
+//Open and close new league form
+function openForm() {
+    document.getElementById("newLeague-form").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("newLeague-form").style.display = "none";
+}
+
+//Checks that passwords match
+function checkLeaguePasswordMatch() {
+    const p1 = document.getElementsByName("lPsw");
+    const p2 = document.getElementsByName("lPswCon");
+    if (p1 != p2) {
+        alert("Passwords do not match");
+        console.log(p1);
+        console.log(p2);
     }
     else {
-        this.textContent = "Light";
-    }*/
-    console.log('current class name: ' + className);
-
-});
+        alert("Passwords match");
+        console.log(p1);
+        console.log(p2);
+    }
+}
